8b8350bcdb8f184cd7bc0fc96d9b66cf045d8f36
Chris Handy <chrisjhandy@gmail.com>
2021-07-14 22:14:04 -0400

    updating styles

--- pages/index.js
+++ pages/index.js
@@ -5,34 +5,34 @@ import { AiFillGithub } from 'react-icons/ai';
 
 export default function Home() {
   return (
-    <div className="font-mono container">
+    <div className="font-mono">
       <Head>
         <title>Chris Handy&apos;s homepage</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className="flex w-full max-w-2xl justify-between p-1 m-1">
-        <div className="flex py-1">
-          <Link href="/">
-            <a className="p-2">chrishandy.net</a>
-          </Link>
-        </div>
-        <div className="flex p-1">
-          {/* <AiFillGithub/> */}
-          <Link href="https://github.com/chandy/chrishandy.net">
-            <a className="p-2">Source</a>
-          </Link>
-            <Link href="https://twitter.com/chrishandy_">
-            <a className="bg-black text-white p-2">Follow Me</a>
+      <header className="flex items-center max-w-2xl justify-between p-1 m-auto">
+          <div className="flex py-1">
+            <Link href="/">
+              <a className="p-2">chrishandy.net</a>
             </Link>
-        </div>
+          </div>
+          <div className="flex p-1">
+            {/* <AiFillGithub/> */}
+            <Link href="https://github.com/chandy/chrishandy.net">
+              <a className="p-2">Source</a>
+            </Link>
+              <Link href="https://twitter.com/chrishandy_">
+              <a className="bg-black text-white p-2">Follow Me</a>
+              </Link>
+          </div>
       </header>
 
-      <main>
+      <main className="flex items-center max-w-2xl justify-center m-auto">
         <div className="flex max-w-2xl justify-center align-middle p-2 m-1">
         <p>Under construction...</p>
         </div>
       </main>
-      <footer>
+      <footer className="flex items-center max-w-2xl justify-center m-auto">
           <div className="flex max-w-2xl justify-center items-center p-2 m-1">
             Powered by{' '}
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
