5008693f95bc8209e54c222e442a67e07c6b389e
Chris Handy <chrisjhandy@gmail.com>
2021-07-14 21:28:55 -0400

    updates

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ import { AiFillGithub } from 'react-icons/ai';
 
 export default function Home() {
   return (
-    <div className="font-mono mx-auto">
+    <div className="font-mono container">
       <Head>
         <title>Chris Handy&apos;s homepage</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className="flex w-full justify-between p-2 m-1">
-        <div className="flex p-1">
+      <header className="flex w-full max-w-2xl justify-between p-1 m-1">
+        <div className="flex py-1">
           <Link href="/">
             <a className="p-2">chrishandy.net</a>
           </Link>
@@ -28,12 +28,12 @@ export default function Home() {
       </header>
 
       <main>
-        <div className="flex justify-center align-middle p-2 m-1">
+        <div className="flex max-w-2xl justify-center align-middle p-2 m-1">
         <p>Under construction...</p>
         </div>
       </main>
       <footer>
-          <div className="flex justify-center items-center p-2 m-1">
+          <div className="flex max-w-2xl justify-center items-center p-2 m-1">
             Powered by{' '}
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </div>
