d8324df2344ea75901ae3e84d3c4955b9f8aa447
Chris Handy <chrisjhandy@gmail.com>
2021-07-16 15:20:55 -0400

    prettier applied to project

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import { AiFillGithub } from 'react-icons/ai';
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { AiFillGithub } from "react-icons/ai";
 
 export default function Home() {
   return (
@@ -34,10 +34,10 @@ export default function Home() {
       </main>
       <footer className="flex items-center max-w-2xl justify-center m-auto">
         <div className="flex max-w-2xl justify-center items-center p-2 m-1">
-          Powered by{' '}
+          Powered by{" "}
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </div>
       </footer>
     </div>
-  )
+  );
 }
