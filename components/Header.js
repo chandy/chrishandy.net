b77daaa11886c199b0bcc50af86c22e395fc2971
Chris Handy <chrisjhandy@gmail.com>
2023-05-27 14:20:48 -0400

    Fixing links

--- components/Header.js
+++ components/Header.js
@@ -3,24 +3,30 @@ import Link from "next/link";
 const Header = () => (
   <header className="flex items-center max-w-2xl justify-between p-1 m-auto">
     <div className="flex p-1">
-      <Link href="/">
-        <a className="p-2">chrishandy.net</a>
+      <Link href="/" className="p-2">
+        chrishandy.net
       </Link>
     </div>
     <div className="flex p-1">
       {/* <AiFillGithub/> */}
-      <Link href="/about">
-        <a className="p-2">About</a>
+      <Link href="/about" className="p-2">
+        About
       </Link>
-      <Link href="https://github.com/chandy/chrishandy.net">
-        <a className="p-2" target="_blank">
+      <Link
+        href="https://github.com/chandy/chrishandy.net"
+        className="p-2"
+        target="_blank">
+        
           Source
-        </a>
+        
       </Link>
-      <Link href="https://twitter.com/chrishandy_">
-        <a className="bg-black text-white p-2" target="_blank">
+      <Link
+        href="https://twitter.com/chrishandy_"
+        className="bg-black text-white p-2"
+        target="_blank">
+        
           Follow Me
-        </a>
+        
       </Link>
     </div>
   </header>
