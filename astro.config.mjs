7015b648605690d6705872dce29ebbb2f8c9f384
Chris Handy <chrisjhandy@gmail.com>
2023-09-11 09:45:23 -0400

    the next chapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx(), vue()]
 });
