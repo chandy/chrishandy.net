843b4de236104f5eeb940f0704de3b926a097e59
Chris Handy <chrisjhandy@gmail.com>
2023-09-20 19:10:10 -0400

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
