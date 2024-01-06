924b5d7069aa1ff0df371d03bf4b0131554945ec
Chris Handy <chrisjhandy@gmail.com>
2024-01-06 14:11:53 -0500

    Fixing head props

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import vue from "@astrojs/vue";
-import htmx from "astro-htmx"
-import node from "@astrojs/node"
+import htmx from "astro-htmx";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
