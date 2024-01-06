bb5f6b70dbfc484b9dc1a4af17fb49dc21e2114c
Chris Handy <chrisjhandy@gmail.com>
2024-01-06 13:40:45 -0500

    Adding htmx and node backend

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import vue from "@astrojs/vue";
+import htmx from "astro-htmx"
+import node from "@astrojs/node"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), vue()]
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+  integrations: [tailwind(), mdx(), vue(), htmx()]
 });
