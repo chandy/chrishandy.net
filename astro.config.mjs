8765d413af99be3b92144e7919fa3b912f0d6eb1
Chris Handy <chrisjhandy@gmail.com>
2024-01-06 16:01:31 -0500

    adding vercel adapter, setting to server mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,15 @@ import vue from "@astrojs/vue";
 import htmx from "astro-htmx";
 import node from "@astrojs/node";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   // output: "server",
   // adapter: node({
   //   mode: "standalone",
   // }),
-  integrations: [tailwind(), mdx(), vue(), htmx()]
+  integrations: [tailwind(), mdx(), vue(), htmx()],
+  output: "server",
+  adapter: vercel()
 });
