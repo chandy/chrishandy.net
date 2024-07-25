e384a4bfae5cb70b8c0ab4e1cf732bc65ab84d42
Chris Handy <chrisjhandy@gmail.com>
2024-07-25 10:15:11 -0400

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import vue from "@astrojs/vue";
 import htmx from "astro-htmx";
-import node from "@astrojs/node";
-
+import alpine from '@astrojs/alpinejs';
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  // output: "server",
-  // adapter: node({
-  //   mode: "standalone",
-  // }),
-  integrations: [tailwind(), mdx(), vue(), htmx()],
   output: "hybrid",
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [tailwind(), mdx(), vue(), htmx(), alpine()]
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,12 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		container: {
+			center: true,
+		},
+		maxWidth: {
+			container: "43rem",
+		}
 	},
 	plugins: [],
 }
